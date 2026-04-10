ba74c5e800797ea0481d2ca86d7abb887d18c48b
Allan Kangethe <amkangethe@outlook.com>
2026-04-10 08:39:05 -0400

    start

---
+++ test-ollama.py
@@ -0,0 +1,53 @@
+import os
+from ollama import Client
+
+# API Key set in terminal
+api_key = os.environ.get('OLLAMA_API_KEY')
+
+# Check for API
+if not api_key:
+    raise ValueError("OLLAMA_API_KEY is not set in this terminal session.")
+
+# Call for ollama model
+client = Client(
+    host='https://ollama.com',
+    headers={'Authorization': f'Bearer {api_key}'}
+)
+#------------------------------------------------------------------------------------------------------------
+# JSON model dialog and background
+messages = [
+    {
+        'role' : 'system',
+        'content' : ('you are a dog')
+     }
+]
+
+while True:
+    userInput = input("\nYou: ").strip()
+    if(userInput.lower() in {'quit'}):  # Break if user enters quit
+        break
+
+    # Update dialog
+    messages.append({
+        'role': 'user',
+        'content': userInput
+    })
+
+    # What the model returns back
+    modelResponse = ""
+
+    # Get model and loop each response into a complete string
+    for part in client.chat(
+        model='gpt-oss:20b',
+        messages=messages,
+        stream=True,
+    ):
+        chunk = part.message.content or ""
+        print(chunk, end="", flush=True) # print the string out as a flush
+        modelResponse += chunk
+
+    messages.append({
+        'role': 'assistant',
+        'content': modelResponse
+    })
+
